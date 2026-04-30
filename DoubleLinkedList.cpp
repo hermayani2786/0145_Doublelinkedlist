7c27149163555b49e4f2427db7993956c5453350
hermayani2786 <herma.yani2786@gmail.com>
2026-04-30 12:11:21 +0700

    tambah validasi duplikasi di awal

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -29,3 +29,26 @@ public:
 
         // Step 1: Allocate memory for new node
         Node *newNode = new Node();
+
+        
+        // Step 2: Assign values
+        newNode->noMhs = nim;
+
+        // Step 3: Insert at beginning if list empty or first element
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+
+            newNode->next = START;
+
+            if (START != NULL)
+                START->prev = newNode;
+
+            newNode->prev = NULL;
+            START = newNode;
+            return;
+        }
